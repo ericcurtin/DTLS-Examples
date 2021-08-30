72ddba8702ec24bab295e9c8cb1add33f76fdd9d
Eric Curtin <ericcurtin17@gmail.com>
2021-08-30 17:33:54 +0100

    changes

--- src/script.sh
+++ src/script.sh
@@ -6,9 +6,9 @@ pkill udp-tls || true
 #gcc -O0 -ggdb udp-tls.c -o udp-tls -lssl -lcrypto
 gcc -O3 udp-tls.c -o udp-tls -lssl -lcrypto -lsctp
 clang-format --style=chromium -i udp-tls.c
-./udp-tls s 127.0.0.1 5000 udp-tls.c &
+./udp-tls s0s 127.0.0.1 5000 udp-tls.c &
 sleep 0.1
-#timeout 1 valgrind ./udp-tls c 127.0.0.1 5000
+./udp-tls c0s 127.0.0.1 5000
 
 pkill udp-tls || true
 
